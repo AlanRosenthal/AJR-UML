b51bc36c7213d2de35e95fe4a5689c15370869f6
Alan Rosenthal <alan.rosenthal@gmail.com>
2013-03-19 21:41:11 -0400

    hw23

--- S2013/DPL/HW23/ErlangBasics/basics.erl
+++ S2013/DPL/HW23/ErlangBasics/basics.erl
@@ -4,21 +4,21 @@
 
 % Define fib(N).
 
-fib(N) -> 5.
+fib(1) -> 1;
+fib(2) -> 1;
+fib(N) -> fib(N-1) + fib(N-2).
 
 
 % Define square_list1(Xs) recursively.
 
-square_list1(Xs) -> lists:reverse(Xs).
-
+square_list1([]) -> [];
+square_list1([X|Xs]) -> 
+    [X*X|square_list1(Xs)].
 
 % Define square_list2(Xs) using lists:map.
 
-square_list2([]) -> 
-    [29];
-
-square_list2([X|Xs]) ->
-    [X+1|square_list1(Xs)].
+square_list2(Xs) ->
+    lists:map(fun(X) -> X*X end,Xs).
 
 
 % Implement map using lists:foldl and one other standard library
@@ -26,10 +26,14 @@ square_list2([X|Xs]) ->
 %   http://www.erlang.org/doc/man/lists.html
 
 map1(Fn, Xs) -> 
-    [].
+    lists:reverse(lists:foldl(fun(X,Acc) -> [Fn(X)|Acc] end, [],Xs)).
 
 
 % Define prime(N) which returns the Nth prime number (prime(1) = 2).
 
 prime(N) ->
-    2.
+    AllPrimes = lists:filter(fun(X) -> basics:isprime(X) end, lists:seq(2,2*N*N,1)),
+    lists:nth(N,AllPrimes).
+    
+isprime(N) ->
+    lists:all(fun(X) -> X /= 0 end,lists:map(fun(X) -> N rem X end,lists:seq(2,N-1,1))).
