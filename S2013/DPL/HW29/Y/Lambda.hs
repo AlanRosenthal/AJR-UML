a6c7549bf8b39f1f68ef5ab7a1f352f972facdc7
Alan Rosenthal <alan@AlanECG.(none)>
2013-04-05 15:45:51 -0400

    progress...

--- S2013/DPL/HW29/Y/Lambda.hs
+++ S2013/DPL/HW29/Y/Lambda.hs
@@ -6,39 +6,26 @@ import Data.List
 
 main = do
     code <- getContents
-    putStrLn $ showLx $ keepsimplify (Number 1000) (replaceSymbolNumber (parseLambda code))
+    putStrLn $ showLx $ keepsimplify $ parseLambda code
 
 fullState = ['a'..'z']
-
-emptyState = []
-
-remState st x =
-    [y | y <- st, y /= x]
-
-addState st x =
-    union st x
+emptyState = ([],[])
+addState st x = union st x
 
 test x = showLx $ simplify $ parseLambda x
-test2 x = showLx $ keepsimplify (Number 1000) (replaceSymbolNumber (parseLambda x))
+test2 x = showLx $ keepsimplify $ parseLambda x
 
-keepsimplify x y =
+keepsimplify x = keepsimplify' (Number 1000) (replaceSymbolNumber x)
+keepsimplify' x y =
 --     if x == (trace (showLx y) y)
     if x == y
        then x
-       else keepsimplify y (simplify y)
-
-test3 x = showLx $ keepaddSymbolNumber (Number 0) (addSymbolNumber $ parseLambda x)
-
-keepaddSymbolNumber x y =
-    if x == y
-       then x
-       else keepaddSymbolNumber y (addSymbolNumber y)
+       else keepsimplify' y (simplify y)
 
 replaceSymbolNumber (Symbol x) =
     case x of
         'I' -> parseLambda "(&x.x)"
         'S' -> parseLambda "(&w.(&y.(&x.y(wyx))))"
---         '+' -> parseLambda "(&x.(&y.x(&w.(&y.(&x.y(wyx))))y))"
         '+' -> replaceSymbolNumber $ parseLambda "(&x.(&y.xSy))"
         '*' -> parseLambda "(&x.(&y.(&z.x(yz))))"
         'T' -> parseLambda "(&xy.x)"
@@ -47,129 +34,30 @@ replaceSymbolNumber (Symbol x) =
         '∨' -> parseLambda "(&xy.x(&uv.u)y)"
         'N' -> parseLambda "(&x.x(&uv.v)(&ab.a))"
         'Z' -> replaceSymbolNumber $ parseLambda "(&x.xFNF)"
---         'P' -> parseLambda "(&n.&f.&x.n(&g.&h.h(gf))(&u.x)(&u.u))"      
+--         'P' -> parseLambda "(&n.&f.&x.n(&g.&h.h(gf))(&u.x)(&u.u))"
         'P' -> replaceSymbolNumber $ parseLambda "(&n.n((&pz.z(S(pT))(pT)))(&z.(z0)0)F)"
         'Y' -> parseLambda "(&g.((&x.g(xx))(&x.g(xx))))"
-        'R' -> replaceSymbolNumber $ parseLambda "(&rn.Zn0(nS(r(Pn))))"
---         'A' -> simplify $ parseLambda "(&rn.Zn1(*n(r(Pn))))"
-        'A' -> replaceSymbolNumber $ parseLambda "Y(&rn.Zn1(*n(r(Pn))))"
+        'R' -> replaceSymbolNumber $ parseLambda "Y(&an.Zn0(+n(a(Pn))))"
+        'A' -> replaceSymbolNumber $ parseLambda "Y(&an.Zn1(*n(a(Pn))))"
+        'L' -> replaceSymbolNumber $ parseLambda "(&a.(&b.(Z(- a b)TF)))"
+        'G' -> replaceSymbolNumber $ parseLambda "(&a.(&b.(Z(- b a)TF)))"
+        'D' -> replaceSymbolNumber $ parseLambda "(&dabc.Z(Ga(*b(Sc))c(d(ab(Sc)))))"
         '-' -> replaceSymbolNumber $ parseLambda "(&x.(&y.yPx))"
         '/' -> error "Division by repeated subtraction (recursive)"
-
 replaceSymbolNumber (Number x) = 
     if (x == 0)
          then parseLambda "(&s.(&z.z))"
          else replaceSymbolNumber (Apply (Symbol 'S') (Number (x-1)))
+replaceSymbolNumber (Apply x y) = (Apply (replaceSymbolNumber x) (replaceSymbolNumber y))
+replaceSymbolNumber (Lambda char body) = (Lambda char (replaceSymbolNumber body))
+replaceSymbolNumber (Name x) = (Name x)
 
-replaceSymbolNumber (Apply x y) =
-    (Apply (replaceSymbolNumber x) (replaceSymbolNumber y))
-
-replaceSymbolNumber (Lambda char body) =
-    (Lambda char (replaceSymbolNumber body))
-
-replaceSymbolNumber (Name x) =
-    (Name x)
-
--- addSymbolNumber l@(Lambda s (Lambda z (Name z'))) =
---     if (z == z')
---        then (Number 0)
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Name x')) =
---     if (x == x')
---        then (Symbol 'I')
---        else l
--- 
--- addSymbolNumber l@(Lambda w (Lambda y (Lambda x (Apply (Name y') (Apply (Apply (Name w') (Name y'')) (Name x')))))) = 
---     if (and [w == w',y == y',y == y'',x == x'])
---        then (Symbol 'S')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Lambda y (Apply (Apply (Name x') (Symbol 'S')) (Name y')))) = 
---     if (and [x == x',y == y'])
---        then (Symbol '+')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Lambda y (Lambda z (Apply (Name x') (Apply (Name y') (Name z')))))) = 
---     if (and [x == x',y == y',z == z'])
---        then (Symbol '*')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Lambda y (Name x'))) = 
---     if (and [x == x'])
---        then (Symbol 'T')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Lambda y (Name y'))) = 
---     if (and [y == y'])
---        then (Symbol 'F')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Lambda y (Apply (Apply (Name x') (Name y')) (Lambda u (Lambda v (Name v')))))) = 
---     if (and [x == x',y == y',v == v'])
---        then (Symbol '∧')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Lambda y (Apply (Apply (Name x') (Lambda u (Lambda v (Name u')))) (Name y')))) = 
---     if (and [x == x',y == y',u == u'])
---        then (Symbol '∨')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Apply (Apply (Name x') (Lambda u (Lambda v (Name v')))) (Lambda a (Lambda b (Name a'))))) = 
---     if (and [x == x',v == v',a == a'])
---        then (Symbol 'N')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Apply (Apply (Apply (Name x') (Symbol 'F')) (Symbol 'N')) (Symbol 'F'))) = 
---     if (and [x == x'])
---        then (Symbol 'Z')
---        else l
--- 
--- addSymbolNumber l@(Lambda x (Apply (Apply (Apply (Name x') (Number 0)) (Symbol 'N')) (Number 0))) = 
---     if (and [x == x'])
---        then (Symbol 'Z')
---        else l
--- 
--- addSymbolNumber l@(Lambda n (Apply (Apply (Apply (Name n') (Lambda p (Lambda z (Apply (Apply (Name z') (Apply (Symbol 'S') (Apply (Name p') (Symbol 'T')))) (Apply (Name p'') (Symbol 'T')))))) (Lambda z''' (Apply (Apply (Name z'''') (Number 0)) (Number 0)))) (Symbol 'F'))) = 
---     if (and [n == n',p == p',p == p'',z == z',z''' == z''''])
---        then (Symbol 'P')
---        else l
--- 
--- addSymbolNumber l@() = 
---     if (and [])
---        then (Symbol 'Y')
---        else l
--- 
--- addSymbolNumber l@() = 
---     if (and [])
---        then (Symbol 'R')
---        else l
-
-
-
-addSymbolNumber l@(Apply (Symbol 'S') (Number x)) = 
-    (Number (x+1))
-addSymbolNumber (Apply x y) =
-    (Apply (addSymbolNumber x) (addSymbolNumber y))
-
-addSymbolNumber (Lambda char body) = 
-    (Lambda char (addSymbolNumber body))
-
-addSymbolNumber (Name x) = 
-    (Name x)
-
-addSymbolNumber (Symbol x) =
-    (Symbol x)
-
-addSymbolNumber (Number x) = 
-    (Number x)
-
-
-simplify (Apply (Lambda char body) tree) = 
---     trace ("\nApply [Lambda {" ++ [char] ++ "} -> " ++ showLx body ++ "] TO " ++ showLx tree) $ replace (Name char') tree body'
+simplify l@(Apply lambda@(Lambda char body) tree) = 
+--     trace ((showLx l) ++ " " ++ (showLx (Apply l' tree))) replace (Name char') tree body'
     replace (Name char') tree body'
-    where st = (free2 tree (emptyState,emptyState))
-          (Lambda char' body') = rename2 (Lambda char body) st (free2 (Lambda char body) (emptyState,emptyState))
+    where
+        (Lambda char' body') = rename2 lambda (free2 tree emptyState) (free2 lambda emptyState)
+--         l'@(Lambda char' body') = rename2 lambda (free2 tree emptyState) (free2 lambda emptyState)
 
 simplify (Apply x y) =
     if (x == x')
@@ -177,109 +65,33 @@ simplify (Apply x y) =
        else (Apply x' y)
     where
         x' = simplify x
-   --     compressApply $ simplifyApply $ expandApply (Apply x y)
---     where expanded = expandApply (Apply x y)
---     trace ("\nApply: " ++ showLx x ++ " TO " ++ showLx y) $ (Apply (simplify x) (simplify y))
---     compressApply $ simplifyApply $ (expandApply (Apply x y))
---     compressApply $ foldl (\a b -> a ++ b) [] $ map expandApply $ simplifyApply $ expandApply (Apply x y)        
+simplify (Lambda char body) = (Lambda char $ simplify body)
+simplify x = x
 
-simplify (Lambda char body) =
---     trace ("\nLambda: {" ++ [char] ++ "} -> " ++ showLx body) $ (Lambda char $ simplify body)
-    (Lambda char $ simplify body)
-
-simplify x = 
---     trace ("\nFALL OFF EDGE: " ++ showLx x) x
-    x
-    
--- rename (Name x) (stF,stB,stU) = 
---     if (elem x stF)
---        then (Name (stU !! (findpos x stF)))
---        else (Name x)
--- 
--- rename (Apply x y) st =
---     (Apply (rename x st) (rename y st))
--- 
--- rename (Lambda char body) (stF,stB,stU) = 
---     if (elem char stF)
---        then (Lambda (stU !! (findpos char stF)) (rename body (stF,stB,stU)))
---        else (Lambda char (rename body (stF,stB,stU)))
-
-expandApply (Apply a b) =
-   (expandApply a) ++ [b]
-   
-expandApply x = [x]
-
-expandApplytest x = expandApply $ parseLambda x
-
-simplifyApply [] = []
-
-simplifyApply ((Lambda char body):x:xs) = 
-    (simplify (Apply (Lambda char body) x)) : simplifyApply xs
-
-simplifyApply ((Apply x y):xs) =
-    (compressApply (simplifyApply (expandApply (Apply x y)))) : simplifyApply xs          
-    
-simplifyApply (x:xs) =
-    x : simplifyApply xs
-    
-compressApply [x] = x
-
-compressApply (xs) =
-    (Apply (compressApply (init xs)) (last xs))
-
-rename2 (Name x) (free,_) (_,bound) =
-    if (elem x bound)
-        then if (elem x free)
-            then (Name (((fullState \\ free) \\ bound) !! (findpos x free)))
-            else (Name x)
+rename2 (Name x) (free,bound) (free',bound') =
+    if (and [elem x bound',elem x free])
+        then (Name (unused !! (findpos x free)))
         else (Name x)
-
-rename2 (Apply x y) st st' =
-    (Apply (rename2 x st st') (rename2 y st st'))
-
-rename2 (Lambda char body) st@(free,_) st'@(_,bound)  =
+    where unused = ((((fullState \\ free) \\ bound) \\ free') \\ bound')
+rename2 (Apply x y) st st' = (Apply (rename2 x st st') (rename2 y st st'))
+rename2 l@(Lambda char body) st@(free,bound) st'@(free',bound')  =
     if (elem char free)
-        then (Lambda (((fullState \\ free) \\ bound) !! (findpos char free)) (rename2 body st st'))
+        then (Lambda (unused !! (findpos char free)) (rename2 body st st'))
         else (Lambda char (rename2 body st st'))
-
-rename2test x y =
-    rename2 (parseLambda x) (free2 (parseLambda y) (emptyState,emptyState)) (free2 (parseLambda x) (emptyState,emptyState))
-
-freetest2 x =
-    free2 (parseLambda x) (emptyState,emptyState)
+    where unused = ((((fullState \\ free) \\ bound) \\ free') \\ bound')
 
 free2 (Name x) (stF,stB) = 
     if (elem x stB)
         then (stF,stB)
         else (addState stF [x],stB)
-
-free2 (Apply x y) st = 
-    free2 y st'
-    where
-        st' = free2 x st
-
-free2 (Lambda char body) (stF,stB) =
-    free2 body (stF,addState stB [char])
-
--- free (Name x) (stF,stB,stU) = 
---     if (elem x stB)
---         then (stF,stB,stU)
---         else (addState stF [x],stB,remState stU x)
--- 
--- free (Apply x y) st = 
---     free x (free y st)
--- 
--- free (Lambda char body) (stF,stB,stU) =
---     free body (stF,addState stB [char],remState stU char)
-
+free2 (Apply x y) st = free2 y (free2 x st)
+free2 (Lambda char body) (stF,stB) = free2 body (stF,addState stB [char])
+    
 replace char repwith (Name x) =
     if ((Name x) == char)
        then repwith
        else (Name x)
-
-replace char repwith (Apply x y) =
-    (Apply (replace char repwith x) (replace char repwith y))
-
+replace char repwith (Apply x y) = (Apply (replace char repwith x) (replace char repwith y))
 replace char repwith (Lambda lchar body) =
     if (char == (Name lchar))
        then (Lambda lchar body)
@@ -291,7 +103,7 @@ findpos x (y:ys) =
        then 0
        else 1 + (findpos x ys)
 
-printlist [] =  return ()
-printlist (x:xs) = 
-    do print x
-       printlist xs
+divisiontest a b c =
+    if (a >= b*(c+1))
+       then divisiontest a b (c + 1)
+       else c
