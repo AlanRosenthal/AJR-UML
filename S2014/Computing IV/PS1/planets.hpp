33df993fba3e11849a836f168f114a6720619160
Alan Rosenthal <alan.rosenthal@gmail.com>
2014-01-27 00:25:25 -0500

    started assignment need to update planets so they print wrt the center

--- S2014/Computing IV/PS1/planets.hpp
+++ S2014/Computing IV/PS1/planets.hpp
@@ -1,6 +1,27 @@
-class Planet {
-    int x;
-    int y;
+#include <SFML/Graphics.hpp>
 
+class Planet { 
+    float pos_x;
+    float pos_y;
+    float vel_x;
+    float vel_y;
+
+    public:
+
+    float mass;
+    sf::CircleShape shape;
+
+    void update_pos(float new_x, float new_y)
+    {
+        pos_x = new_x;
+        pos_y = new_y;
+//      shape.setPosition(new_x,new_y);
+    };
+
+    void update(void)
+    {
+        float km_per_pixel = 5e9;
+        shape.setPosition(pos_x/km_per_pixel+250,pos_y/km_per_pixel+250);
+    };
 
 };
