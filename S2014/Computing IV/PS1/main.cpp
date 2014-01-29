de3db622076ed432b27ccae640a39e11f852238d
AlanRosenthal <alan.rosenthal@gmail.com>
2014-01-29 16:03:12 -0500

    removed commected code

--- S2014/Computing IV/PS1/main.cpp
+++ S2014/Computing IV/PS1/main.cpp
@@ -44,46 +44,7 @@ int main(int argc, char* argv[])
 
     sf::RenderWindow window(sf::VideoMode(500,500),"Solar System");
     window.setFramerateLimit(60);
-
-    int i;
-    for (i = 0;i < 3;i++)
-    {
     
-    }
-
-/*    Universe::Planet sun, mercury, venus, earth, mars;
-
-    sun.shape = sf::CircleShape(20.f);
-    sun.shape.setFillColor(sf::Color::Yellow);
-    sun.pos_x = 0;
-    sun.pos_y = 0;
-    sun.mass= 1.9890e+30;
-    
-    mercury.shape = sf::CircleShape(10.f);
-    mercury.shape.setFillColor(sf::Color::Green);
-    mercury.pos_x = 5.7900e+10;
-    mercury.pos_y = 0;
-    mercury.mass = 3.3020e+23;
-    
-    venus.shape = sf::CircleShape(10.f);
-    venus.shape.setFillColor(sf::Color::Magenta);
-    venus.pos_x = 1.0820e+11;
-    venus.pos_y = 0;
-    venus.mass = 4.8690e+24;
-    
-    earth.shape = sf::CircleShape(10.f);
-    earth.shape.setFillColor(sf::Color::Blue);
-    earth.pos_x = 1.4960e+11;
-    earth.pos_y = 0;
-    earth.mass = 5.9740e+24;
-    
-    mars.shape = sf::CircleShape(10.f);
-    mars.shape.setFillColor(sf::Color::Red);
-    mars.pos_x = 2.2790e+11;
-    mars.pos_y = 0;
-    mars.mass = 6.4190e+23;
-*/
-
     while (window.isOpen())
     {
         sf::Event event;
