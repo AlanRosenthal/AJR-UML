93a49146c4f0ff77de4abf5d54267957d5b767b4
AlanRosenthal <alan.rosenthal@gmail.com>
2014-03-17 15:07:10 -0400

    Inital commit for A2

---
+++ S2014/OS2/A2/chprio.c
@@ -0,0 +1,27 @@
+/* chprio.c - chprio */
+
+#include <conf.h>
+#include <kernel.h>
+#include <proc.h>
+
+/*------------------------------------------------------------------------
+ * chprio  --  change the scheduling priority of a process
+ *------------------------------------------------------------------------
+ */
+SYSCALL chprio(int pid, int newprio)
+{
+	int	oldprio;
+	struct	pentry	*pptr;
+        char    ps;
+
+	disable(ps);
+	if (isbadpid(pid) || newprio<=0 ||
+	    (pptr = &proctab[pid])->pstate == PRFREE) {
+		restore(ps);
+		return(SYSERR);
+	}
+	oldprio = pptr->pprio;
+	pptr->pprio = newprio;
+	restore(ps);
+	return(oldprio);
+}
